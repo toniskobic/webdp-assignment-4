8d5b2f556e0d1986eb32568d8c4483b4382dbf30
TONIPC\Toni <tskobic@foi.hr>
2021-05-25 01:55:48 +0200

    4. zadaća WebDiP: -made changes to work locally

--- public_html/backup.php
+++ public_html/backup.php
@@ -6,17 +6,14 @@ require "$direktorij/sesija.class.php";
 
 Sesija::kreirajSesiju();
 
-if (empty($_SERVER['HTTP_REFERER'])) {
-    header("Location: ./obrasci/prijava.php");
-}
-
 if(isset($_GET['napravi'])) {
-    //$command = "***REMOVED***";
     $command = "***REMOVED***";
+    //$command = "***REMOVED***";
     exec($command);
 
     $_SESSION['file'] = 'datoteke/backup.sql';
 
+    /*
     $mail = $_GET['email'];
     $polje = explode("@", $mail);
     $username = $polje[0];
@@ -27,17 +24,19 @@ if(isset($_GET['napravi'])) {
     $mail_body = 'Sigurnosna kopija napravljena!';
 
     mail($mail_to, $mail_subject, $mail_body, $mail_from);
+    */
     
     header("Location: ./obrasci/obrazac.php");
 }
 if(isset($_GET['vrati'])) {
     if(!empty($_SESSION['file'])) {
         $file = $_SESSION['file'];
-        //$command = "***REMOVED***";
-        $command = "mysql -u 'WebDiP2020x119' -p'12345' WebDiP2020x119 < {$file}";
+        $command = "***REMOVED***";
+        //$command = "mysql -u 'WebDiP2020x119' -p'12345' WebDiP2020x119 < {$file}";
         exec($command);
         unlink ($file); 
         
+        /*
         $mail = $_GET['email'];
         $polje = explode("@", $mail);
         $username = $polje[0];
@@ -48,7 +47,8 @@ if(isset($_GET['vrati'])) {
         $mail_body = 'Podaci preuzeti iz sigurnosne kopije!';
     
         mail($mail_to, $mail_subject, $mail_body, $mail_from);
-        
+        */
+
         $_SESSION['file'] = '';
     }
     header("Location: ./obrasci/obrazac.php");
